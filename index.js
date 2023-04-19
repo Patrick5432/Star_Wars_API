d700f4ab4faaaa3b589f834595e6b90755fc0014
Maxim <maxx5326@gmail.com>
2023-04-19 11:57:55 +0500

    Запросы sqlite

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const bodyparser = require("body-parser")
+const sqlite3 = require("sqlite3")
 require("dotenv").config()
 
 const app = express()
+const sqlite3 = require("sqlite3")
+const db = new sqlite3.Database('./db/Star_wars_swapi.db')
 
-app.use(bodyparser.urlencoded({ extended: false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 
@@ -12,6 +15,33 @@ app.use(bodyparser.json())
 app.get('/', (req, res) => {
     console.log(req.query)
 })
+
+app.get('./planets', (res, req) => {
+
+
+
+    db.each("SELECT = FROM planets", (err, planets) => {
+        planets.push({ ...planets, residents: [] })
+        res.json(planets)
+    })
+
+    // const result = await db.all("SELECT = FROM planets")
+})
+
+app.get('/people/:id', (req, res) => {
+    const peopleId = req.params.id
+    db.all(`SELECT = FROM people where id=${peopleId}`, (err, people) => {
+        res.json(people[0])
+    })
+})
+
+
+
+
+
+
+
+
 
 
 app.listen(process.env.PORT, () => {
